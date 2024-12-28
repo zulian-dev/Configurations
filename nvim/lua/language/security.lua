df40588127449b0dc4695baec1f394bed99d9e66
Zulian <luiz.zulian@kovi.com.br>
2024-12-27 21:08:32 -0300

    security sonar as tool

--- nvim/lua/language/security.lua
+++ nvim/lua/language/security.lua
@@ -18,6 +18,7 @@ local enable_solar_languages = {
   "javascript",
   "java",
   "json",
+  "elixir",
 }
 
 --------------------------------------------------------------------------------
@@ -32,8 +33,8 @@ security.plugins = {
     dependencies = {
       "mfussenegger/nvim-jdtls",
     },
-    ft = enable_solar_languages,
-  },
+    -- ft = enable_solar_languages,
+  }
 }
 
 --------------------------------------------------------------------------------
@@ -49,55 +50,80 @@ security.mason = {
 -- LSP -------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
-security.lsp = function(lspconfig, capabilities, on_attach)
-  -- lspconfig.snyk_ls.setup({
-  -- 	capabilities = capabilities,
-  -- 	on_attach = on_attach,
-  --	filetypes = enable_snyk_languages,
-  -- })
+-- security.lsp = function(lspconfig, capabilities, on_attach)
+--   lspconfig.snyk_ls.setup({
+--     capabilities = capabilities,
+--     on_attach = on_attach,
+--     filetypes = enable_snyk_languages,
+--     -- cmd = {"/Users/kovi/.local/share/nvim/mason/bin/snyk-ls", "-f", "/Users/kovi/Documents/git/Configurations/nvim/logs/snyk-ls-vim.log", },
+--     root_dir = function(name)
+--       return lspconfig.util.find_git_ancestor(name) or vim.loop.os_homedir()
+--     end,
+--
+--     init_options = {
+--       activateSnykCode = "true",
+--     },
+--   })
+--
+-- local sonarlintAnalisers = {}
 
-  -- local sonarlintAnalisers = {}
+-- if sonarlintAnalisers["java"] then
+--   table.insert(
+--     sonarlintAnalisers,
+--     vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarjava.jar")
+--   )
+-- -- end
+--
+--   require("sonarlint").setup({
+--     server = {
+--       cmd = {
+--         "sonarlint-language-server",
+--         -- Ensure that sonarlint-language-server uses stdio channel
+--         "-stdio",
+--         "-analyzers",
+--         vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarjava.jar"),
+--         vim.fn.expand("$MASON/share/sonarlint-analyzers/sonargo.jar"),
+--         vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarhtml.jar"),
+--         vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarxml.jar"),
+--         vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarjs.jar"),
+--         vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarphp.jar"),
+--         vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarpython.jar"),
+--         vim.fn.expand("$MASON/share/sonarlint-analyzers/sonariac.jar"), -- docker, k8s, terraform
+--         vim.fn.expand("$MASON/share/sonarlint-analyzers/sonar-elixir-plugin-1.0-SNAPSHOT.jar"),
+--
+--         -- ls ~/.local/share/nvim/mason/share/sonarlint-analyzers/
+--         -- sonarcfamily.jar@
+--         -- sonarhtml.jar@
+--         -- sonarjava.jar@
+--         -- sonarjs.jar@
+--         -- sonarphp.jar@
+--         -- sonartext.jar@
+--         -- sonargo.jar@
+--         -- sonariac.jar@
+--         -- sonarjavasymbolicexecution.jar@
+--         -- sonarlintomnisharp.jar@
+--         -- sonarpython.jar@
+--         -- sonarxml.jar@
+--       },
+--     },
+--     filetypes = enable_solar_languages,
+--   })
+-- end
 
-  -- if sonarlintAnalisers["java"] then
-  --   table.insert(
-  --     sonarlintAnalisers,
-  --     vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarjava.jar")
-  --   )
-  -- end
 
-  require("sonarlint").setup({
-    server = {
-      cmd = {
-        "sonarlint-language-server",
-        -- Ensure that sonarlint-language-server uses stdio channel
-        "-stdio",
-        "-analyzers",
-        vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarjava.jar"),
-        vim.fn.expand("$MASON/share/sonarlint-analyzers/sonargo.jar"),
-        vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarhtml.jar"),
-        vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarxml.jar"),
-        vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarjs.jar"),
-        vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarphp.jar"),
-        vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarpython.jar"),
-        vim.fn.expand("$MASON/share/sonarlint-analyzers/sonariac.jar"), -- docker, k8s, terraform
-
-        -- ls ~/.local/share/nvim/mason/share/sonarlint-analyzers/
-        -- sonarcfamily.jar@
-        -- sonarhtml.jar@
-        -- sonarjava.jar@
-        -- sonarjs.jar@
-        -- sonarphp.jar@
-        -- sonartext.jar@
-        -- sonargo.jar@
-        -- sonariac.jar@
-        -- sonarjavasymbolicexecution.jar@
-        -- sonarlintomnisharp.jar@
-        -- sonarpython.jar@
-        -- sonarxml.jar@
-      },
-    },
-    filetypes = enable_solar_languages,
-  })
-end
+security.tools = {
+  enable_sonarlint_analyzer = function(analyzer)
+    require("sonarlint").setup({
+      server = {
+        cmd = {
+          "sonarlint-language-server",
+          "-stdio",
+          "-analyzers",
+          analyzer,
+        }
+      }
+    })
+  end,
+}
 
 return security
