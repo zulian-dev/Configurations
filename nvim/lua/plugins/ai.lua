460aab94ba7d1d94d4b2d2b821020e14d2119d1e
Zulian <luiz.zulian@kovi.com.br>
2025-02-09 23:48:49 -0300

    configurando o avante

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -46,7 +46,7 @@ return {
 
   {
     "github/copilot.vim",
-    enabled = true,
+    enabled = false,
     -- 'zbirenbaum/copilot.lua',
     config = function()
       vim.api.nvim_set_keymap("i", "<c-p>", "<Plug>(copilot-panel)", { noremap = true, silent = true })
@@ -119,6 +119,110 @@ return {
       "nvim-lua/plenary.nvim",
       "folke/trouble.nvim",
       "nvim-telescope/telescope.nvim",
+    },
+  },
+
+  -- avante.nvim is a Neovim plugin designed to emulate the behaviour of
+  -- the Cursor AI IDE. It provides users with AI-driven code suggestions
+  -- and the ability to apply these recommendations directly to their source
+  -- files with minimal effort.
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = false,
+    version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+
+    opts = {
+      behaviour = {
+        auto_suggestions = false, -- Experimental stage
+        auto_set_highlight_group = true,
+        auto_set_keymaps = true,
+        auto_apply_diff_after_generation = false,
+        support_paste_from_clipboard = false,
+        minimize_diff = true,     -- Whether to remove unchanged lines when applying a code block
+        enable_token_counting = true, -- Whether to enable token counting. Default to true.
+      },
+
+      provider = "ollama",
+      vendors = {
+        ollama = {
+          api_key_name = "",
+          ask = true,
+          endpoint = "http://127.0.0.1:11434/api",
+          model = "deepseek-r1",
+          parse_curl_args = function(opts, code_opts)
+            -- require("config.utils").notify.debug("code_opts", code_opts)
+            return {
+              url = opts.endpoint .. "/chat",
+              headers = {
+                ["Accept"] = "application/json",
+                ["Content-Type"] = "application/json",
+              },
+              body = {
+                model = opts.model,
+                options = {
+                  num_ctx = 16384,
+                },
+                messages = require("avante.providers").copilot.parse_messages(code_opts), -- you can make your own message, but this is very advanced
+                -- max_tokens = 2048,
+                stream = true,
+              },
+            }
+          end,
+
+          parse_stream_data = function(data, handler_opts)
+            -- Parse the JSON data
+            local json_data = vim.fn.json_decode(data)
+            -- Check if the response contains a message
+            if json_data and json_data.message and json_data.message.content then
+              -- Extract the content from the message
+              local content = json_data.message.content
+              -- Call the handler with the content
+              handler_opts.on_chunk(content)
+            end
+          end,
+        },
+      },
+    },
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = "make",
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    dependencies = {
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "echasnovski/mini.pick",      -- for file_selector provider mini.pick
+      "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+      "hrsh7th/nvim-cmp",           -- autocompletion for avante commands and mentions
+      "ibhagwan/fzf-lua",           -- for file_selector provider fzf
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "zbirenbaum/copilot.lua",     -- for providers='copilot'
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
     },
   },
 }
