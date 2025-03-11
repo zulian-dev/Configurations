d9b400fc9a95f7c38d16cf800671520adf916422
Zulian <luiz@zulian.dev>
2025-03-11 11:14:39 -0300

    vim add indent rainbow

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -37,99 +37,121 @@ return {
   {
     "jbgutierrez/vim-better-comments",
   },
-  -- Indentation rainbown
+  -- Indentation rainbow
   {
     "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    opts = function(_, opts)
+      -- Other blankline configuration here
+      return require("indent-rainbowline").make_opts(opts, {
+        color_transparency = 0.15,
+        colors = {
+          0x9400D3, -- Violeta escuro
+          0x4B0082, -- Índigo
+          0x0000FF, -- Azul
+          0x00FF00, -- Verde
+          0xFFFF00, -- Amarelo
+          0xFF7F00, -- Laranja
+          0xFF0000, -- Vermelho
+        },
+      })
+    end,
     dependencies = {
       "TheGLander/indent-rainbowline.nvim",
     },
-    event = "LazyFile",
-    main = "ibl",
-    opts = function(_, opts)
-      Snacks.toggle({
-        name = "Indention Guides",
-        get = function()
-          return require("ibl.config").get_config(0).enabled
-        end,
-        set = function(state)
-          require("ibl").setup_buffer(0, { enabled = state })
-        end,
-      }):map("<leader>ug")
-
-      local highlight = {
-        "RainbowRed",
-        "RainbowYellow",
-        "RainbowBlue",
-        "RainbowOrange",
-        "RainbowGreen",
-        "RainbowViolet",
-        "RainbowCyan",
-      }
-      local hooks = require("ibl.hooks")
-      -- create the highlight groups in the highlight setup hook, so they are reset
-      -- every time the colorscheme changes
-      hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-        vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
-        vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-        vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-        vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-        vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-        vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-        vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
-      end)
-
-      vim.g.rainbow_delimiters = { highlight = highlight }
-      require("ibl").setup({ scope = { highlight = highlight } })
-
-      hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
-
-      require("indent-rainbowline").make_opts(opts, {
-        -- How transparent should the rainbow colors be. 1 is completely opaque, 0 is invisible. 0.07 by default
-        color_transparency = 0.15,
-        -- The 24-bit colors to mix with the background. Specified in hex.
-        -- { 0xffff40, 0x79ff79, 0xff79ff, 0x4fecec, } by default
-        colors = {
-          0xff0000, -- Vermelho
-          0xff7f00, -- Laranja
-          0xffff00, -- Amarelo
-          0x00ff00, -- Verde
-          0x0000ff, -- Azul
-          0x4b0082, -- Índigo
-          0x9400d3, -- Violeta
-        },
-      })
-
-      return {
-        indent = {
-          char = "│",
-          tab_char = "│",
-        },
-        scope = {
-          show_start = true,
-          show_end = true,
-          highlight = highlight,
-        },
-        exclude = {
-          filetypes = {
-            "Trouble",
-            "alpha",
-            "dashboard",
-            "help",
-            "lazy",
-            "mason",
-            "neo-tree",
-            "notify",
-            "snacks_dashboard",
-            "snacks_notif",
-            "snacks_terminal",
-            "snacks_win",
-            "toggleterm",
-            "trouble",
-          },
-        },
-      }
-    end,
   },
+  -- {
+  --   "lukas-reineke/indent-blankline.nvim",
+  --   dependencies = {
+  --     "TheGLander/indent-rainbowline.nvim",
+  --   },
+  --   event = "LazyFile",
+  --   main = "ibl",
+  --   opts = function(_, opts)
+  --     Snacks.toggle({
+  --       name = "Indention Guides",
+  --       get = function()
+  --         return require("ibl.config").get_config(0).enabled
+  --       end,
+  --       set = function(state)
+  --         require("ibl").setup_buffer(0, { enabled = state })
+  --       end,
+  --     }):map("<leader>ug")
+
+  --     local highlight = {
+  --       "RainbowRed",
+  --       "RainbowYellow",
+  --       "RainbowBlue",
+  --       "RainbowOrange",
+  --       "RainbowGreen",
+  --       "RainbowViolet",
+  --       "RainbowCyan",
+  --     }
+  --     local hooks = require("ibl.hooks")
+  --     -- create the highlight groups in the highlight setup hook, so they are reset
+  --     -- every time the colorscheme changes
+  --     hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+  --       vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+  --       vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+  --       vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+  --       vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+  --       vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+  --       vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+  --       vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+  --     end)
+
+  --     vim.g.rainbow_delimiters = { highlight = highlight }
+  --     require("ibl").setup({ scope = { highlight = highlight } })
+
+  --     hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+
+  --     require("indent-rainbowline").make_opts(opts, {
+  --       -- How transparent should the rainbow colors be. 1 is completely opaque, 0 is invisible. 0.07 by default
+  --       color_transparency = 0.15,
+  --       -- The 24-bit colors to mix with the background. Specified in hex.
+  --       -- { 0xffff40, 0x79ff79, 0xff79ff, 0x4fecec, } by default
+  --       colors = {
+  --         0xff0000, -- Vermelho
+  --         0xff7f00, -- Laranja
+  --         0xffff00, -- Amarelo
+  --         0x00ff00, -- Verde
+  --         0x0000ff, -- Azul
+  --         0x4b0082, -- Índigo
+  --         0x9400d3, -- Violeta
+  --       },
+  --     })
+
+  --     return {
+  --       indent = {
+  --         char = "│",
+  --         tab_char = "│",
+  --       },
+  --       scope = {
+  --         show_start = true,
+  --         show_end = true,
+  --         highlight = highlight,
+  --       },
+  --       exclude = {
+  --         filetypes = {
+  --           "Trouble",
+  --           "alpha",
+  --           "dashboard",
+  --           "help",
+  --           "lazy",
+  --           "mason",
+  --           "neo-tree",
+  --           "notify",
+  --           "snacks_dashboard",
+  --           "snacks_notif",
+  --           "snacks_terminal",
+  --           "snacks_win",
+  --           "toggleterm",
+  --           "trouble",
+  --         },
+  --       },
+  --     }
+  --   end,
+  -- },
   {
     "tomasky/bookmarks.nvim",
     enabled = false,
