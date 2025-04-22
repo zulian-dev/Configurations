3698a1696bf978af5561d6c47a92bbea4ac3b9e1
Zulian <luiz@zulian.dev>
2025-04-22 08:57:08 -0300

    nvim update ai initial prompt

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -33,200 +33,199 @@ Output format:
 - Security best practices (if relevant).
 ]]
 
-
 return {
-  {
-    "github/copilot.vim",
-    enabled = true,
-    -- 'zbirenbaum/copilot.lua',
-    config = function()
-      vim.api.nvim_set_keymap("i", "<c-p>", "<Plug>(copilot-panel)", { noremap = true, silent = true })
-      vim.keymap.set("n", "<Leader>cs", "<cmd>Copilot status<CR>", { desc = "Copilot status" })
-    end,
-  },
+	{
+		"github/copilot.vim",
+		enabled = true,
+		-- 'zbirenbaum/copilot.lua',
+		config = function()
+			vim.api.nvim_set_keymap("i", "<c-p>", "<Plug>(copilot-panel)", { noremap = true, silent = true })
+			vim.keymap.set("n", "<Leader>cs", "<cmd>Copilot status<CR>", { desc = "Copilot status" })
+		end,
+	},
 
-  {
-    "olimorris/codecompanion.nvim",
-    opts = {},
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
-    },
-    config = function()
-      local codecompanion = require("codecompanion")
-      local config = require("codecompanion.config")
+	{
+		"olimorris/codecompanion.nvim",
+		opts = {},
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		config = function()
+			local codecompanion = require("codecompanion")
+			local config = require("codecompanion.config")
 
-      local original_system_prompt = config.opts.system_prompt({
-        language = "Portuguese",
-      })
+			local original_system_prompt = config.opts.system_prompt({
+				language = "Portuguese",
+			})
 
-      vim.keymap.set({ "n", "v" }, "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
-      vim.keymap.set(
-        { "n", "v" },
-        "<LocalLeader>a",
-        "<cmd>CodeCompanionChat Toggle<cr>",
-        { noremap = true, silent = true }
-      )
-      vim.keymap.set("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
+			vim.keymap.set({ "n", "v" }, "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
+			vim.keymap.set(
+				{ "n", "v" },
+				"<Leader>ca",
+				"<cmd>CodeCompanionChat Toggle<cr>",
+				{ noremap = true, silent = true }
+			)
+			vim.keymap.set("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
 
-      -- Expand 'cc' into 'CodeCompanion' in the command line
-      vim.cmd([[cab cc CodeCompanion]])
+			-- Expand 'cc' into 'CodeCompanion' in the command line
+			vim.cmd([[cab cc CodeCompanion]])
 
-      codecompanion.setup({
-        opts = {
-          system_prompt = function(opts)
-            return table.concat({
-              original_system_prompt,
-              codecompanion_comportamental,
-              codecompanion_security,
-            }, "\n\n")
-          end,
-        },
-      })
-    end,
-  },
+			codecompanion.setup({
+				opts = {
+					system_prompt = function(opts)
+						return table.concat({
+							original_system_prompt,
+							codecompanion_comportamental,
+							codecompanion_security,
+						}, "\n\n")
+					end,
+				},
+			})
+		end,
+	},
 
-  {
-    "jackMort/ChatGPT.nvim",
-    enabled = false,
-    event = "VeryLazy",
-    config = function()
-      require("chatgpt").setup({
-        api_key_cmd = "op read op://Personal/OpenAI/API-KEY",
-      })
+	{
+		"jackMort/ChatGPT.nvim",
+		enabled = false,
+		event = "VeryLazy",
+		config = function()
+			require("chatgpt").setup({
+				api_key_cmd = "op read op://Personal/OpenAI/API-KEY",
+			})
 
-      local wk = require("which-key")
-      wk.add({
-        { "c",  group = "ChatGPT" },
-        { "cc", "<cmd>ChatGPT<CR>", desc = "ChatGPT" },
-        {
-          mode = { "n", "v" },
-          { "ca", "<cmd>ChatGPTRun add_tests<CR>",                 desc = "Add Tests" },
-          { "cd", "<cmd>ChatGPTRun docstring<CR>",                 desc = "Docstring" },
-          { "ce", "<cmd>ChatGPTEditWithInstruction<CR>",           desc = "Edit with instruction" },
-          { "cf", "<cmd>ChatGPTRun fix_bugs<CR>",                  desc = "Fix Bugs" },
-          { "cg", "<cmd>ChatGPTRun grammar_correction<CR>",        desc = "Grammar Correction" },
-          { "ck", "<cmd>ChatGPTRun keywords<CR>",                  desc = "Keywords" },
-          { "cl", "<cmd>ChatGPTRun code_readability_analysis<CR>", desc = "Code Readability Analysis" },
-          { "co", "<cmd>ChatGPTRun optimize_code<CR>",             desc = "Optimize Code" },
-          { "cr", "<cmd>ChatGPTRun roxygen_edit<CR>",              desc = "Roxygen Edit" },
-          { "cs", "<cmd>ChatGPTRun summarize<CR>",                 desc = "Summarize" },
-          { "ct", "<cmd>ChatGPTRun translate<CR>",                 desc = "Translate" },
-          { "cx", "<cmd>ChatGPTRun explain_code<CR>",              desc = "Explain Code" },
-        },
-      })
-    end,
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "nvim-lua/plenary.nvim",
-      "folke/trouble.nvim",
-      "nvim-telescope/telescope.nvim",
-    },
-  },
+			local wk = require("which-key")
+			wk.add({
+				{ "c", group = "ChatGPT" },
+				{ "cc", "<cmd>ChatGPT<CR>", desc = "ChatGPT" },
+				{
+					mode = { "n", "v" },
+					{ "ca", "<cmd>ChatGPTRun add_tests<CR>", desc = "Add Tests" },
+					{ "cd", "<cmd>ChatGPTRun docstring<CR>", desc = "Docstring" },
+					{ "ce", "<cmd>ChatGPTEditWithInstruction<CR>", desc = "Edit with instruction" },
+					{ "cf", "<cmd>ChatGPTRun fix_bugs<CR>", desc = "Fix Bugs" },
+					{ "cg", "<cmd>ChatGPTRun grammar_correction<CR>", desc = "Grammar Correction" },
+					{ "ck", "<cmd>ChatGPTRun keywords<CR>", desc = "Keywords" },
+					{ "cl", "<cmd>ChatGPTRun code_readability_analysis<CR>", desc = "Code Readability Analysis" },
+					{ "co", "<cmd>ChatGPTRun optimize_code<CR>", desc = "Optimize Code" },
+					{ "cr", "<cmd>ChatGPTRun roxygen_edit<CR>", desc = "Roxygen Edit" },
+					{ "cs", "<cmd>ChatGPTRun summarize<CR>", desc = "Summarize" },
+					{ "ct", "<cmd>ChatGPTRun translate<CR>", desc = "Translate" },
+					{ "cx", "<cmd>ChatGPTRun explain_code<CR>", desc = "Explain Code" },
+				},
+			})
+		end,
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+			"nvim-lua/plenary.nvim",
+			"folke/trouble.nvim",
+			"nvim-telescope/telescope.nvim",
+		},
+	},
 
-  -- avante.nvim is a Neovim plugin designed to emulate the behaviour of
-  -- the Cursor AI IDE. It provides users with AI-driven code suggestions
-  -- and the ability to apply these recommendations directly to their source
-  -- files with minimal effort.
-  {
-    "yetone/avante.nvim",
-    event = "VeryLazy",
-    enabled = false,
-    lazy = false,
-    version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+	-- avante.nvim is a Neovim plugin designed to emulate the behaviour of
+	-- the Cursor AI IDE. It provides users with AI-driven code suggestions
+	-- and the ability to apply these recommendations directly to their source
+	-- files with minimal effort.
+	{
+		"yetone/avante.nvim",
+		event = "VeryLazy",
+		enabled = false,
+		lazy = false,
+		version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
 
-    opts = {
-      behaviour = {
-        auto_suggestions = false, -- Experimental stage
-        auto_set_highlight_group = true,
-        auto_set_keymaps = true,
-        auto_apply_diff_after_generation = false,
-        support_paste_from_clipboard = false,
-        minimize_diff = true,         -- Whether to remove unchanged lines when applying a code block
-        enable_token_counting = true, -- Whether to enable token counting. Default to true.
-      },
+		opts = {
+			behaviour = {
+				auto_suggestions = false, -- Experimental stage
+				auto_set_highlight_group = true,
+				auto_set_keymaps = true,
+				auto_apply_diff_after_generation = false,
+				support_paste_from_clipboard = false,
+				minimize_diff = true, -- Whether to remove unchanged lines when applying a code block
+				enable_token_counting = true, -- Whether to enable token counting. Default to true.
+			},
 
-      provider = "ollama",
-      vendors = {
-        ollama = {
-          api_key_name = "",
-          ask = true,
-          endpoint = "http://127.0.0.1:11434/api",
-          model = "deepseek-r1",
-          -- model = "codegemma",
-          parse_curl_args = function(opts, code_opts)
-            -- require("config.utils").notify.debug("code_opts", code_opts)
-            return {
-              url = opts.endpoint .. "/chat",
-              headers = {
-                ["Accept"] = "application/json",
-                ["Content-Type"] = "application/json",
-              },
-              body = {
-                model = opts.model,
-                options = {
-                  num_ctx = 16384,
-                },
-                messages = require("avante.providers").copilot.parse_messages(code_opts), -- you can make your own message, but this is very advanced
-                -- max_tokens = 2048,
-                stream = true,
-              },
-            }
-          end,
+			provider = "ollama",
+			vendors = {
+				ollama = {
+					api_key_name = "",
+					ask = true,
+					endpoint = "http://127.0.0.1:11434/api",
+					model = "deepseek-r1",
+					-- model = "codegemma",
+					parse_curl_args = function(opts, code_opts)
+						-- require("config.utils").notify.debug("code_opts", code_opts)
+						return {
+							url = opts.endpoint .. "/chat",
+							headers = {
+								["Accept"] = "application/json",
+								["Content-Type"] = "application/json",
+							},
+							body = {
+								model = opts.model,
+								options = {
+									num_ctx = 16384,
+								},
+								messages = require("avante.providers").copilot.parse_messages(code_opts), -- you can make your own message, but this is very advanced
+								-- max_tokens = 2048,
+								stream = true,
+							},
+						}
+					end,
 
-          parse_stream_data = function(data, handler_opts)
-            -- Parse the JSON data
-            local json_data = vim.fn.json_decode(data)
-            -- Check if the response contains a message
-            if json_data and json_data.message and json_data.message.content then
-              -- Extract the content from the message
-              local content = json_data.message.content
-              -- Call the handler with the content
-              handler_opts.on_chunk(content)
-            end
-          end,
-        },
-      },
-    },
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    build = "make",
-    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-    dependencies = {
-      "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      --- The below dependencies are optional,
-      "echasnovski/mini.pick",         -- for file_selector provider mini.pick
-      "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-      "hrsh7th/nvim-cmp",              -- autocompletion for avante commands and mentions
-      "ibhagwan/fzf-lua",              -- for file_selector provider fzf
-      "nvim-tree/nvim-web-devicons",   -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua",        -- for providers='copilot'
-      {
-        -- support for image pasting
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {
-          -- recommended settings
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
-            -- required for Windows users
-            use_absolute_path = true,
-          },
-        },
-      },
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        "MeanderingProgrammer/render-markdown.nvim",
-        opts = {
-          file_types = { "markdown", "Avante" },
-        },
-        ft = { "markdown", "Avante" },
-      },
-    },
-  },
+					parse_stream_data = function(data, handler_opts)
+						-- Parse the JSON data
+						local json_data = vim.fn.json_decode(data)
+						-- Check if the response contains a message
+						if json_data and json_data.message and json_data.message.content then
+							-- Extract the content from the message
+							local content = json_data.message.content
+							-- Call the handler with the content
+							handler_opts.on_chunk(content)
+						end
+					end,
+				},
+			},
+		},
+		-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+		build = "make",
+		-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+		dependencies = {
+			"stevearc/dressing.nvim",
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			--- The below dependencies are optional,
+			"echasnovski/mini.pick", -- for file_selector provider mini.pick
+			"nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+			"hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+			"ibhagwan/fzf-lua", -- for file_selector provider fzf
+			"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+			"zbirenbaum/copilot.lua", -- for providers='copilot'
+			{
+				-- support for image pasting
+				"HakonHarnes/img-clip.nvim",
+				event = "VeryLazy",
+				opts = {
+					-- recommended settings
+					default = {
+						embed_image_as_base64 = false,
+						prompt_for_file_name = false,
+						drag_and_drop = {
+							insert_mode = true,
+						},
+						-- required for Windows users
+						use_absolute_path = true,
+					},
+				},
+			},
+			{
+				-- Make sure to set this up properly if you have lazy=true
+				"MeanderingProgrammer/render-markdown.nvim",
+				opts = {
+					file_types = { "markdown", "Avante" },
+				},
+				ft = { "markdown", "Avante" },
+			},
+		},
+	},
 }
